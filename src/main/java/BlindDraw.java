725e43380257b280ac8ec64aabc3df9d54ee0729
wle831 <newuser@example.com>
2014-05-08 15:06:55 -0700

    changed a lot of things; -made two seperate arrayLists(one for initial player list, the second to hold the teams) -made a method that takes the initial player list and passes them to the team arrayList and assigns each team a seed

--- src/main/java/BlindDraw.java
+++ src/main/java/BlindDraw.java
@@ -2,9 +2,23 @@ import java.util.*;
 
 public class BlindDraw {
 
+    public static ArrayList<String> playerList = new ArrayList<String>(); //initial list of players
+    public static ArrayList<Team> teamList = new ArrayList<Team>(); //stores instances of team objects
+
+
     public static void main(String[] args) {
-        Team.makeTeam("Will", "Mark");
-        Team.makeTeam("Devin", "Kyle");
-        System.out.println(Team.list);
+
+        playerList.add("Will"); //add players
+        playerList.add("Mark");
+        playerList.add("Ben");
+        playerList.add("Dennis");
+        playerList.add("Dave");
+        playerList.add("Bryan");
+
+        Collections.shuffle(playerList); // shuffler initial playerlist
+        Team.makeTeams(playerList); // puts players into teams and assigns them seeds
+
+        System.out.println(teamList); //prints out the team array with seeds
+
     }
 }
--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,41 +1,57 @@
 import java.util.*;
+import java.util.Collections;
 
 /**
  * Created by wevans on 5/6/14.
  */
 public class Team {
 
-    //initialize our variables for the class
     String p1 = "";
     String p2 = "";
     int seed = 0;
     char bracket = 'W';
 
-    //creates array to hold our objects (teams)
-    public static ArrayList<Team> list = new ArrayList<Team>();
-
-    public Team() {
+    public Team() { //object constructor
         this.p1 = "";
         this.p2 = "";
         this.seed = 0;
         this.bracket = 'W';
     }
 
-    public Team(String p1, String p2, int seed, char bracket) {
+    public Team(String p1, String p2, int seed, char bracket) { //object constructor
         this.p1 = p1;
         this.p2 = p2;
         this.seed = seed;
         this.bracket = bracket;
     }
-
     @Override
-    public String toString() {
+    public String toString() { // prints out more pretty like when calling an instance object of Team
         return "{ " + p1 + ", " + p2 + ", " + seed + ", " + bracket + " }";
     }
 
-    public static void makeTeam(String p1, String p2) {
-        list.add(new Team(p1, p2, 0, 'W')); //adds a team to the arraylist
+    public int getSeed() { //gets seed int of current instance of object team
+        return seed;
     }
 
+    public void setSeed(int seed) { //change seed int of current instance of object team
+        this.seed = seed;
+    }
+
+    public ArrayList setTeamSeeds(ArrayList list) {
+        for (int i = 0; i < list.size(); i++) {
+            setSeed(i);
+        }
+        return list;
+    }
+
+    public static ArrayList makeTeams(ArrayList playerList) {
+        Team t;
+        for (int i = 0; i < playerList.size() - 1; i++) {
+
+            BlindDraw.teamList.add(t = new Team(playerList.get(i).toString(), playerList.get(i + 1).toString(), 0, 'W'));
+            t.setSeed(i + 1);
+        }
+        return BlindDraw.teamList;
+    }
 }
 
