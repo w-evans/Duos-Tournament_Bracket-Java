686ab63e100b61f4c82d156f2d870a01fca3663a
wle831 <newuser@example.com>
2014-05-08 16:12:50 -0700

    fix matchup add

---
+++ src/main/java/MatchUp.java
@@ -0,0 +1,18 @@
+/**
+ * Created by wevans on 5/8/14.
+ */
+public class MatchUp {
+    //initial variables
+    Object t1;
+    Object t2;
+
+    public MatchUp() { //object constructor with defaults
+        this.t1 = BlindDraw.teamList.get(0);
+        this.t2 = BlindDraw.teamList.get(1);
+    }
+
+    public MatchUp(Object t1, Object t2) { //object constructor
+        this.t1 = t1;
+        this.t2 = t2;
+    }
+}
