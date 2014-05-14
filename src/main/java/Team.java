6e2693e4747a5592a23c3fa9eb036dc39bb45829
10a16 <anon@anon.com>
2014-05-13 19:54:53 -0700

    -removed comments

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -33,19 +33,4 @@ public class Team {
     public void setSeed(int seed) { //change seed int of current instance of object team
         this.seed = seed;
     }
-/*
-    public static ArrayList makeTeams(ArrayList playerList) {
-        Team t;
-        int count = 1; //for setting seeds
-
-        for (int i = 1; i < playerList.size(); i += 2) {
-            //creates new instance of team and passes to new array + adding players from player list array
-            BlindDraw.teamList.add(t = new Team(playerList.get(i - 1).toString(), playerList.get(i).toString(), 0, 'W'));
-
-            t.setSeed(count); //sets seed of current instance of team
-            count = count + 1; //increments count by 1 every time an instance of team is created
-        }
-        return BlindDraw.teamList;
-    }
-  */
 }
