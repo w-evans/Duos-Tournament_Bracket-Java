e53a7a38547b8c6437af42211e5adec33cb7dbc1
wle831 <newuser@example.com>
2014-05-21 14:31:22 -0700

    -working new logic on bracket

--- src/main/java/WinBracket.java
+++ src/main/java/WinBracket.java
@@ -13,34 +13,30 @@ public class WinBracket {
 
             //match W1
             //team 1/team 2
-                                    //match W5
-                                    //winner of t1/t2 + t3/4
+            MatchUp.doBattle(Bracket.teamList.get(0), Bracket.teamList.get(1));
+
             //match W2
-            //team 3/team 4                                                                             //merge
-                                                                    //match W7                          //match W8
-                                                                    //winner of W5/winner W6            //winner of L6/winner W7
+            //team 3/team 4                                                                                                //merge
+            MatchUp.doBattle(Bracket.teamList.get(2), Bracket.teamList.get(3));                                            //match W7                      //match W8
+                                                                                       //winner of W5/winner W6            //winner of L6/winner W7
             //match W3
             //team 5/team 6
-                                   //match W6
-                                   //winner of t5/t6 + t7/t8
+            MatchUp.doBattle(Bracket.teamList.get(4), Bracket.teamList.get(5));
+
             //match W4
             //team7/team8
+            MatchUp.doBattle(Bracket.teamList.get(6), Bracket.teamList.get(7));
 
 
-            //losers bracket
+            //round 2
 
-            //match L1
-            //loser of match W1/loser of match W2
-                                                    //match L3
-                                                    //loser match W5/winner match L1
-                                                                                                //match L5
-                                                                                                //winner of L3/winner L4
-                                                                                                                            //match L6
-                                                                                                                            // loser W7/winner L5     //merge bracket
-            //match L2
-                //loser of match W3/loser of match W4
-                                                    //match L4
-                                                    //loser match W6/winner match L2
+            //match W5
+            //winner of t1/t2 + t3/4
+            MatchUp.doBattle(Bracket.teamList.get(0), Bracket.teamList.get(1));
+
+            //match W6
+            //winner of W3 + W4
+
 
         }
     }
