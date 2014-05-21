9e73bb7feb11a6f9fec98a37204e2a5eaad198ee
wle831 <newuser@example.com>
2014-05-21 15:04:39 -0700

    -losers bracket logic done (8-man)

--- src/main/java/LoserBracket.java
+++ src/main/java/LoserBracket.java
@@ -6,20 +6,65 @@ public class LoserBracket {
 
     public static void RoundOne(ArrayList loserList) {
 
-        if(Bracket.teamList.size() == 8) {
+        //4 teams in winners, 4 in losers
+        if(Bracket.teamList.size() == 4 && Bracket.loserList.size() == 4) {
+
         //match L1
         //loser of match W1/loser of match W2
+        MatchUp.doBattle(Bracket.loserList.get(0), Bracket.loserList.get(1));
+         //winner gets seed of 1
+         //loser eliminated
 
-                                                //match L3
-                                                //loser match W5/winner match L1
-                                                                                        //match L5
-                                                                                        //winner of L3/winner L4
-                                                                                                                     //match L6
-                                                                                                                    // loser W7/winner L5      //merge new match W8
-                                                //match L4
-                                                //loser match W6/winner match L2
         //match L2
         //loser of match W3/loser of match W4
+        MatchUp.doBattle(Bracket.loserList.get(2), Bracket.loserList.get(3));
+        //winner gets seed of 3
+        //loser eliminated
+
+        }
+    }
+
+    public static void RoundTwo(ArrayList loserList) {
+
+        if(Bracket.teamList.size() == 2 && Bracket.loserList.size() == 4) { //2 teams eliminated, 2 in winners, 4 in losers
+
+            //match L3
+            //loser match W5/winner match L1
+            //seeds 1 & 2
+            MatchUp.doBattle(Bracket.loserList.get(0), Bracket.loserList.get(1));
+            //winner gets seed of 1
+
+
+            //match L4
+            //loser match w6/winner match L2
+            //seeds 3 & 4
+            MatchUp.doBattle(Bracket.loserList.get(2), Bracket.loserList.get(3));
+            //winner gets seed of 2
+
+        }
+
+    }
+
+    public static void RoundThree(ArrayList loserList) {
+        if(Bracket.loserList.size() == 2 && Bracket.teamList.size() == 1) { // 4 teams eliminated, 1 in winners, 2 in losers
+
+            //match L5
+            //winner of L3 and winner L4
+            //seeds 1 & 2
+            MatchUp.doBattle(Bracket.loserList.get(0), Bracket.loserList.get(1));
+            //winner gets seed of 1
+        }
+
+    }
+
+    public static void RoundFour(ArrayList loserList) {
+        if(Bracket.loserList.size() == 2 && Bracket.teamList.size() == 1) { // 5 teams eliminated, 2 in losers, 1 in winners
+
+            //match L6
+            //loser W7/winner L5
+            //seeds 1 & 2
+            MatchUp.doBattle(Bracket.loserList.get(0), Bracket.loserList.get(0));
+            //winner goes to final match
         }
     }
 }
